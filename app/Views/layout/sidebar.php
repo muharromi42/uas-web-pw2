f867389bd17d2e7339e669bc1e9d2d895693caab
muharromi42 <muharromi42@gmail.com>
2024-07-02 09:02:34 +0700

    perbaikan sidebar dan navbar

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="Logo MI" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?= base_url('img/logomi.png'); ?>"" alt=" Logo MI" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Pempek IF</span>
     </a>
 
